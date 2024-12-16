8d433402a7ceebd07db1d0cdbb762a63d908c566
jihoon <leejh080628@naver.com>
2024-12-16 16:50:40 +0900

    feat :: baseVC 추가

---
+++ Projects/Core/Sources/Base/BaseViewController.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+open class BaseViewController: UIViewController {
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+        attribute()
+        bind()
+        bindAction()
+    }
+    open override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        addView()
+        layout()
+    }
+    open func attribute() {
+    }
+    open func bind() {
+    }
+    open func bindAction() {
+    }
+    open func addView() {
+    }
+    open func subscribe() {
+    }
+    open func layout() {
+    }
+}
