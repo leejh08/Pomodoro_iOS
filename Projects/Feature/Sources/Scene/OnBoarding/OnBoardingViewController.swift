4b62e899ea42ff81faba1d11e1686dd1a4195f6b
jihoon <leejh080628@naver.com>
2024-12-26 23:09:30 +0900

    chore :: OnBoardingVM 추가

--- Projects/Feature/Sources/Scene/OnBoarding/OnBoardingViewController.swift
+++ Projects/Feature/Sources/Scene/OnBoarding/OnBoardingViewController.swift
@@ -1,1 +1,15 @@
+import UIKit
 
+import SnapKit
+import Then
+
+import RxSwift
+import RxCocoa
+
+import Core
+import DesignSystem
+
+public class OnboardingViewController: BaseViewController<OnboardingViewModel> {
+    
+    
+}
---
+++ Projects/Feature/Sources/Scene/OnBoarding/OnBoardingViewModel.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+import RxSwift
+import RxCocoa
+
+
+
+public class OnboardingViewModel: BaseViewModel {
+    
+    private let disposeBag = DisposeBag()
+}
