e18a96502ff503b2a54483834b6049d1cbcb2732
jihoon <leejh080628@naver.com>
2024-12-29 12:57:03 +0900

    setting :: 모듈에 sources 추가

--- Projects/Presentation/Project.swift
+++ Projects/Presentation/Project.swift
@@ -4,7 +4,7 @@ import DependencyPlugin
 
 let project = Project.makeModule(
     name: "Presentation",
-    resources: [],
+    resources: ["Resources/**"], // 리소스 경로 추가
     platform: .iOS,
     product: .staticFramework,
     dependencies: [
---
+++ Projects/Presentation/Sources/Scene/OnBoarding/OnBoarding.swift
@@ -0,0 +1,8 @@
+//
+//  OnBoarding.swift
+//  Presentation
+//
+//  Created by 이지훈 on 12/29/24.
+//  Copyright © 2024 com.jihoon.id. All rights reserved.
+//
+
